3fd31b76cf81ba051c86791bcb327546c178fc97
root <root@ip-172-31-22-8.us-east-2.compute.internal>
2019-02-24 15:46:26 +0000

    Java File

---
+++ emp.java
@@ -0,0 +1,27 @@
+public class emp {
+
+	int eid;
+	String ename;
+	float esal;
+	emp (int eid,String ename,float esal)
+	{
+		this.eid=eid;
+		this.ename=ename;
+		this.esal=esal;
+		
+	}
+	void disp()
+	{
+		System.out.println("empid="+eid);
+		System.out.println("ename="+ename);
+		System.out.println("esal="+esal);
+	}	
+		public static void main(String[] args)
+		{
+	emp e1=new emp(111 , "Amiya" , 1000);
+	e1.disp();
+	emp e2=new emp(222,"Lipu",2000);
+	e2.disp();
+			}
+
+}
---
+++ test.java
@@ -0,0 +1,9 @@
+public class Testproj {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+		System.out.println("We are going to create a project");
+	}
+	
+}
